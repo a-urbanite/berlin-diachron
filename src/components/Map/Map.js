cfa522046be6f3dc332e749efc1790c27d1cbe7b
a.urbanite <a.urbanite@gmail.com>
2022-10-21 14:32:15 +0200

    corrected attribution links

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -10,9 +10,11 @@ import 'leaflet.fullscreen/Control.FullScreen.css'
 
 const Map = () => {
   return (
+    
     <MapContainer 
-      center={[52.5200, 13.4050]} 
-      zoom={13} 
+    center={[52.5200, 13.4050]} 
+    zoom={13} 
+        // default crs L.CRS.EPSG3857
       // crs={L.CRS.EPSG4326}
       scrollWheelZoom={false} 
       className={styles.mapContainer}
@@ -49,21 +51,21 @@ const Map = () => {
             <WMSTileLayer
               url="https://fbinter.stadt-berlin.de/fb/wms/senstadt/k_luftbild1928?"
               params={{layers: '0'}}
-              attribution={'<a href="https://fbinter.stadt-berlin.de/fb/index.jsp?Szenario=luftbild&loginkey=zoomStart&mapId=k_luftbild1928@senstadt">Geoportal Berlin, Luftbilder 1928</a>'}
+              attribution={'<a target="_blank" href="https://fbinter.stadt-berlin.de/fb/index.jsp?Szenario=luftbild&loginkey=zoomStart&mapId=k_luftbild1928@senstadt">Geoportal Berlin, Luftbilder 1928</a>'}
             />
         </LayersControl.Overlay>
         <LayersControl.Overlay name="1953">
             <WMSTileLayer
               url="https://fbinter.stadt-berlin.de/fb/wms/senstadt/k_luftbild1953"
               params={{layers: '0'}}
-              attribution={'<a href="<a href="https://fbinter.stadt-berlin.de/fb/index.jsp?Szenario=luftbild&loginkey=zoomStart&mapId=k_luftbild1953@senstadt&bbox=358608,5798720,427354,5838813">Geoportal Berlin, Luftbilder 1953</a>'}
+              attribution={'<a target="_blank" href="https://fbinter.stadt-berlin.de/fb/index.jsp?Szenario=luftbild&loginkey=zoomStart&mapId=k_luftbild1953@senstadt">Geoportal Berlin, Luftbilder 1953</a>'}
             />
         </LayersControl.Overlay>
         <LayersControl.Overlay name="1992-97">
             <WMSTileLayer
               url="https://isk.geobasis-bb.de/ows/dop50g_1992_1997_wms?"
               params={{layers: 'bb_DOP50g_1992_1997'}}
-              attribution={'<a href="https://geobroker.geobasis-bb.de/gbss.php?MODE=GetProductPreview&PRODUCTID=06f356f2-d61f-4bdf-9c5b-1741b273b327">© GeoBasis-DE/LGB, dl-de/by-2-0</a>'}
+              attribution={'<a target="_blank" href="https://geobroker.geobasis-bb.de/gbss.php?MODE=GetProductPreview&PRODUCTID=06f356f2-d61f-4bdf-9c5b-1741b273b327">© GeoBasis-DE/LGB, dl-de/by-2-0</a>'}
             />
         </LayersControl.Overlay>
       </LayersControl>
