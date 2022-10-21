1aeec173735245d5fad0300c270ea9b012b77b04
a.urbanite <a.urbanite@gmail.com>
2022-10-21 15:43:08 +0200

    added icon to locateMe and made layer mutually exclusive

--- src/components/Map/LocateControl/LocateControl.js
+++ src/components/Map/LocateControl/LocateControl.js
@@ -13,8 +13,12 @@ export default function LocateControl() {
       position: 'topleft',
       maxZoom: 19,
       strings: {
-          title: 'Show me where I am, yo!'
+          title: 'Show me where I am!'
       },
+      flyTo: true,
+      drawCircle: true,
+      icon: "locateIcon",
+      iconElementTag: 'div',
       onActivate: () => {} // callback before engine starts retrieving locations
     }
 
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -33,22 +33,22 @@ const Map = () => {
       />
       <LocateControl/>
       <LayersControl position="topright">
-        <LayersControl.Overlay name="1750">
+        <LayersControl.BaseLayer name="1750">
             <TileLayer
               url="http://warper.wmflabs.org/maps/tile/188/{z}/{x}/{y}.png"
             />
-        </LayersControl.Overlay>
-        <LayersControl.Overlay name="1804">
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="1804">
             <TileLayer
               url="http://warper.wmflabs.org/maps/tile/468/{z}/{x}/{y}.png"
             />
-        </LayersControl.Overlay>
-        <LayersControl.Overlay name="1863">
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="1863">
             <TileLayer
               url="http://warper.wmflabs.org/maps/tile/287/{z}/{x}/{y}.png"
             />
-        </LayersControl.Overlay>
-        <LayersControl.Overlay name="1928">
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="1928">
             <WMSTileLayer
               url="https://fbinter.stadt-berlin.de/fb/wms/senstadt/k_luftbild1928?"
               params={{layers: '0'}}
@@ -63,22 +63,22 @@ const Map = () => {
               //   }
               // }}
             />
-        </LayersControl.Overlay>
-        <LayersControl.Overlay name="1953">
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="1953">
             <WMSTileLayer
               url="https://fbinter.stadt-berlin.de/fb/wms/senstadt/k_luftbild1953"
               params={{layers: '0'}}
               crs={L.CRS.EPSG4326}
               attribution={'<a target="_blank" href="https://fbinter.stadt-berlin.de/fb/index.jsp?Szenario=luftbild&loginkey=zoomStart&mapId=k_luftbild1953@senstadt">Geoportal Berlin, Luftbilder 1953</a>'}
             />
-        </LayersControl.Overlay>
-        <LayersControl.Overlay name="1992-97">
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="1992-97">
             <WMSTileLayer
               url="https://isk.geobasis-bb.de/ows/dop50g_1992_1997_wms?"
               params={{layers: 'bb_DOP50g_1992_1997'}}
               attribution={'<a target="_blank" href="https://geobroker.geobasis-bb.de/gbss.php?MODE=GetProductPreview&PRODUCTID=06f356f2-d61f-4bdf-9c5b-1741b273b327">© GeoBasis-DE/LGB, dl-de/by-2-0</a>'}
             />
-        </LayersControl.Overlay>
+        </LayersControl.BaseLayer>
       </LayersControl>
     </MapContainer>
   )
