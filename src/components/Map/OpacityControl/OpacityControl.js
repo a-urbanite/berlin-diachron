dd03f287b88aa3afdeee866b386268a78c4ba4d9
a.urbanite <a.urbanite@gmail.com>
2022-10-24 18:20:55 +0200

    opacity control now collapsible and with icon

--- src/components/Map/OpacityControl/OpacityControl.js
+++ src/components/Map/OpacityControl/OpacityControl.js
@@ -13,7 +13,7 @@ const OpacityControl = () => {
   
   useEffect(() => {
     if (controlLayer !== null && controlLayer.name !== 'default') {
-      L.control.opacity({"opacity": controlLayer.layer}, {position: 'bottomright'}).addTo(map);
+      L.control.opacity({"opacity": controlLayer.layer}, {position: 'bottomright', collapsed: true}).addTo(map);
     }
     return () => {
       map._controlContainer.children[3].children[0]?.remove()
